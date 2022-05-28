cd4adcb62b72606926f4094e9dd394f37f82f822
angelorc <angelo@rcstart.it>
2022-05-28 17:31:20 +0200

    call cmdRoot from main

--- bitsong/main.go
+++ bitsong/main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"github.com/angelorc/sinfonia-go/bitsong/chain"
-	"github.com/angelorc/sinfonia-go/bitsong/indexer"
-	"github.com/angelorc/sinfonia-go/config"
-	"github.com/angelorc/sinfonia-go/mongo/db"
-	"log"
+	"github.com/angelorc/sinfonia-go/bitsong/cmd/sinfonia-bitsong/cmd"
+	"os"
 )
 
 func main() {
-	/**
-	 * Connect to db
-	 */
-	defaultDB := db.Database{
-		DataBaseRefName: "default",
-		URL:             config.GetSecret("MONGO_URI"),
-		DataBaseName:    config.GetSecret("MONGO_DATABASE"),
-		RetryWrites:     config.GetSecret("MONGO_RETRYWRITES"),
+	if err := cmd.NewRootCmd().Execute(); err != nil {
+		os.Exit(1)
 	}
-	defaultDB.Init()
-	defer defaultDB.Disconnect()
-
-	client, err := chain.NewClient(chain.GetBitsongConfig())
-	if err != nil {
-		log.Fatalf("failed to get RPC endpoints on chain %s. err: %v", "bitsong", err)
-	}
-
-	indexer.NewIndexer(client).Parse(1, 1000, 10)
 }
